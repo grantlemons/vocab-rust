56d7ffacceae237b63a4820b8ff5d0cf710dd60d
Grant Lemons <grant0lemons@gmail.com>
2023-01-31 18:01:30 -0600

    basic bin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+//! Retrieves definitions etc. from wordreference api and assembles them into a table
+#![warn(missing_docs)]
+
+#[allow(unused_imports)]
+use wordreference as wr;
+
+/// Entrypoint for binary program
+#[tokio::main]
+async fn main() -> Result<(), ()> {
+    println!(
+        "{:#?}",
+        wr::get_def("inutil".to_string(), None, None).await?
+    );
+    Ok(())
 }
