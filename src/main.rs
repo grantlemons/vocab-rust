2c545e6be7a7e126d8b846b70334b12ce1813db1
Grant Lemons <grant0lemons@gmail.com>
2023-02-03 11:39:40 -0600

    refactor main into functions

--- src/main.rs
+++ src/main.rs
@@ -10,47 +10,66 @@ use wordreference::{Definition, Response};
 /// Entrypoint for binary program
 #[tokio::main]
 async fn main() -> Result<(), ()> {
-    let mut stdout = stdout();
-    let mut possible_definitions: Vec<Response> = Vec::new();
+    let mut words: Vec<Response> = Vec::new();
     let mut chosen_definitions: Vec<Definition> = Vec::new();
 
     clear_term();
-    stdout.execute(cursor::MoveTo(0, 0)).unwrap();
+    choose_words(&mut words).await?;
+    for word in words {
+        clear_term();
+        stdout().execute(cursor::MoveTo(0, 0)).unwrap();
+        choose_definition(&word, &mut chosen_definitions);
+    }
+    clear_term();
+    print_definitions(&chosen_definitions);
+
+    Ok(())
+}
+
+fn clear_term() {
+    stdout()
+        .queue(terminal::Clear(terminal::ClearType::All))
+        .unwrap();
+}
+
+async fn choose_words(words: &mut Vec<Response>) -> Result<(), ()> {
+    stdout().execute(cursor::MoveTo(0, 0)).unwrap();
     loop {
-        print!("Word {}: ", possible_definitions.len() + 1);
+        print!("Word {}: ", words.len() + 1);
         let input: String = read!("{}\n");
 
         if !input.is_empty() {
-            possible_definitions.push(wr::get_def(input, None, None).await?)
+            let definitions = wr::get_def(input, None, None).await?;
+            words.push(definitions);
         } else {
             break;
         }
     }
-    clear_term();
+    Ok(())
+}
 
-    for res in possible_definitions {
-        stdout.execute(cursor::MoveTo(0, 0)).unwrap();
-        println!("Definition options for {}", res.definitions[0].from.word);
-        println!("========================================================================");
-        for (index, def) in res.definitions.iter().enumerate() {
-            println!(
-                "{}: {} ({})  --  {}  --  \"{}\" / \"{}\"",
-                index + 1,
-                def.from.word,
-                def.from.part,
-                def.from.definition,
-                def.from.example,
-                def.to.example
-            );
-        }
-        print!("Index: ");
-        let input: usize = read!("{}\n");
-
-        chosen_definitions.push(res.definitions.get(input - 1).unwrap().clone());
-        clear_term();
+fn choose_definition(word: &Response, chosen_definitions: &mut Vec<Definition>) {
+    println!("Definition options for {}", word.definitions[0].from.word);
+    println!("========================================================================");
+    for (index, def) in word.definitions.iter().enumerate() {
+        println!(
+            "{}: {} ({})  --  {}  --  \"{}\" / \"{}\"",
+            index + 1,
+            def.from.word,
+            def.from.part,
+            def.from.definition,
+            def.from.example,
+            def.to.example
+        );
     }
+    print!("Index: ");
+    let input: usize = read!("{}\n");
 
-    stdout.execute(cursor::MoveTo(0, 0)).unwrap();
+    chosen_definitions.push(word.definitions.get(input - 1).unwrap().clone());
+}
+
+fn print_definitions(chosen_definitions: &Vec<Definition>) {
+    stdout().execute(cursor::MoveTo(0, 0)).unwrap();
     println!(
         "{:<4}{:<25}{:<4}|{:^20}|{:^20}|{:^40}|{:^20}|",
         "", "Palabra", "", "Categoría", "Fuente", "Definición y diccionario", "Contexto"
@@ -71,12 +90,4 @@ async fn main() -> Result<(), ()> {
             ""
         );
     }
-
-    Ok(())
-}
-
-fn clear_term() {
-    stdout()
-        .queue(terminal::Clear(terminal::ClearType::All))
-        .unwrap();
 }
