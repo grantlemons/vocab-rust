9165caf5cd2ffbca72f4173ff75984d8f0da9fdc
Grant Lemons <grant0lemons@gmail.com>
2023-02-03 16:51:51 -0600

    add test for random spanish words

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -171,6 +171,26 @@ mod tests {
     }
 
     #[tokio::test]
+    async fn test_random_words() -> Result<(), String> {
+        /* cspell: disable */
+        let words: &[&str] = &[
+            "ladrillo",
+            "destilación",
+            "prolongado",
+            "alto",
+            "verte",
+            "hace",
+            "ya",
+            "puedo",
+        ];
+        /* cspell: enable */
+        for word in words {
+            crate::get_def(word.to_string(), None, None).await?;
+        }
+        Ok(())
+    }
+
+    #[tokio::test]
     async fn test_invalid_word() -> Result<(), String> {
         match crate::get_def("sjfadohjfahndkllhjra".to_string(), None, None).await {
             Ok(_) => Err("Expected error".to_string()),
@@ -179,11 +199,10 @@ mod tests {
     }
 
     #[tokio::test]
-    #[ignore]
-    async fn english_to_spanish() -> Result<(), String> {
+    async fn english_to_spanish_brick() -> Result<(), String> {
         match crate::get_def("brick".to_string(), None, None).await {
             Ok(res) => {
-                assert_eq!(res.definitions.len(), 3);
+                assert_eq!(res.definitions.len(), 7);
                 Ok(())
             }
             Err(err) => Err(err),
